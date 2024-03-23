13ca54391e8b442a1ca2e6c47efacdaff93ca5c3
FelipeNegrucci <felipe.negrucci@protonmail.com>
2024-03-23 19:44:37 -0300

    commit

--- src/components/card.vue
+++ src/components/card.vue
@@ -21,7 +21,7 @@ const props = defineProps({
             <div class="card-value fs-5">{{ props.data[1] }}</div>
             <div>
                 <span class="card-new fw-bold me-1">{{ props.data[2] }} novos</span>
-                <span>registros (mes)</span>
+                <span class="card-registry">registros (mes)</span>
             </div>
         </div>
         <div class="col-auto px-0">
@@ -60,9 +60,9 @@ const props = defineProps({
         &-new{
             color: var(--bs-success);
         }
-        &-icon{
-            background-color: rgba(var(--bs-primary-rgb), .15);
-        }    
+        &-registry{
+            color: var(--bs-gray-600);
+        }
     }
 }
 [data-bs-theme="dark"]{
@@ -79,9 +79,9 @@ const props = defineProps({
         &-new{
             color: var(--bs-success);
         }
-        &-icon{
-            background-color: rgba(var(--bs-primary-rgb), .2);
-        }    
+        &-registry{
+            color: var(--bs-gray-500);
+        }
     }
 }
 </style>
