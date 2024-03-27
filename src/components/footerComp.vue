d206df9263084afb22ed6ebe562c0d41c0f4b0b2
FelipeNegrucci <felipe.negrucci@protonmail.com>
2024-03-27 13:39:41 -0300

    commit

--- src/components/footerComp.vue
+++ src/components/footerComp.vue
@@ -1,17 +1,95 @@
 <script setup>
+import iconChart from './icons/iconChart.vue';
 
+const props = defineProps({
+    display:{
+        width:{
+            required: true,
+            type: Object
+        },
+        isLandscapeOrientation:{
+            required: true,
+            type: Boolean
+        },
+        isMobileDevice:{
+            required:true,
+            type: Boolean
+        }
+    }
+})
 </script>
 
 <template>
-    <div class="container-fluid">
+    <footer class="container-fluid py-4">
         <div class="container-xxl">
-            <div class="row">
-                <div class="col px-0">footer</div>
+            <div class="row" :class="props.display.width < 625 ? 'justify-content-center' : 'justify-content-between'">
+                <div class="col-auto text-color px-0 text-center" :class="props.display.width < 625 ? 'mb-2' : 'my-auto'">© 2024-2025 Felipe Negrucci. Todos os direitos reservados.</div>
+                <div class="col-auto px-0 hstack gap-1 my-auto">
+                    <RouterLink to="/" class="icon-area rounded-2 d-flex">
+                        <iconChart class="icon-color m-auto"/>
+                    </RouterLink>
+                    <RouterLink to="/" class="icon-area rounded-2 d-flex">
+                        <iconChart class="icon-color m-auto"/>
+                    </RouterLink>
+                    <RouterLink to="/" class="icon-area rounded-2 d-flex">
+                        <iconChart class="icon-color m-auto"/>
+                    </RouterLink>
+                    <RouterLink to="/" class="icon-area rounded-2 d-flex">
+                        <iconChart class="icon-color m-auto"/>
+                    </RouterLink>
+                </div>
             </div>
         </div>
-    </div>
+    </footer>
 </template>
 
-<style>
-
+<style scoped lang="scss">
+.icon-area{
+    width: 36px;
+    height: 36px;
+}
+.icon-color{
+    width: 20px;
+    height: 20px;
+}
+[data-bs-theme='light']{
+    footer{
+        .text-color{
+            color: rgba(var(--bs-dark-rgb), 0.7);
+        }
+        color: var(--bs-dark);
+        background-color: var(--bs-light);
+        .icon-area{
+            .icon-color{
+                fill: rgba(var(--bs-dark-rgb), 0.7);
+            }
+            &:hover{
+                background-color: rgba(var(--bs-dark-rgb), 0.07);
+                .icon-color{
+                    fill: var(--bs-dark);
+                }
+            }
+        }
+    }
+}
+[data-bs-theme='dark']{
+    footer{
+        .text-color{
+            color: rgba(var(--bs-light-rgb), 0.7);
+        }
+        color: var(--bs-light);
+        background-color: var(--bs-dark);
+        .icon-area{
+            .icon-color{
+                fill: rgba(var(--bs-light-rgb), 0.7);
+            }
+            &:hover{
+                background-color: rgba(var(--bs-light-rgb), 0.07);
+                .icon-color{
+                    fill: var(--bs-light);
+                }
+            }
+        }
+    }
+}
 </style>
